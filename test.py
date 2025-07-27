0a3fb234b2372a0cc911a5316777fae77cff1492
Kyle Allen Abandia <148830902+IKairuu@users.noreply.github.com>
2025-07-27 20:24:41 +0800

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import google.generativeai as genai
-import os
-gemini_api_key = os.getenv("API")
-genai.configure(api_key=gemini_api_key)
-model = genai.GenerativeModel("gemini-2.5-pro")
-
-user_input = input("Input Question: ")
-response = model.generate_content(user_input)
-print(response.text)
