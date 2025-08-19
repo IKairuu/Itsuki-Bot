0eb3b8e3a73d4685f375fadc03f7f60d2fc4403e
Kyle Allen Abandia <148830902+IKairuu@users.noreply.github.com>
2025-08-19 10:20:46 +0800

    Update main.py
    Updated the structure of the main.py

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ from background_cog import Background
 activity = discord.Game(name="&help to interrupt Itsuki")
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="&", activity=activity,intents=intents, help_command=None)
-
+guilds = []
 
 async def setup():
     await bot.add_cog(HelpCog(bot))
-    await bot.add_cog(Background(bot))
+    await bot.add_cog(Background(bot, guilds))
+        
+    await bot.start(os.getenv('TOKEN'))
 
 asyncio.run(setup())
-bot.run(os.getenv('TOKEN'))
