a25e2c048ff37f6db13688b46800bef4d1e3a84a
Kyle Allen Abandia <148830902+IKairuu@users.noreply.github.com>
2025-08-24 11:55:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,10 @@ from background_cog import Background
 activity = discord.Game(name="&help to interrupt Itsuki")
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="&", activity=activity,intents=intents, help_command=None)
-guilds = []
 
 async def setup():
     await bot.add_cog(HelpCog(bot))
-    await bot.add_cog(Background(bot, guilds))
-
+    await bot.add_cog(Background(bot))
+        
 asyncio.run(setup())
 bot.run(os.getenv('TOKEN'))
